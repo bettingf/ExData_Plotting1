f307dd6a0757689c4531b5c20eda924ff74d086c
franck <Franck Bettinger>
2015-06-07 13:42:03 +0200

    plot 3 done

--- plot3.R
+++ plot3.R
@@ -4,4 +4,10 @@ subdata$Date<-as.Date(subdata$Date,"%d/%m/%Y")
 subdata$DateTime<-strptime(paste(subdata$Date, subdata$Time), "%Y-%m-%d %H:%M:%S")
 subdata$Global_active_power<-as.numeric(subdata$Global_active_power)
 
-mdata<-melt(subdata, id.vars=c("DateTime"), measure.vars=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), value.name="Sub_metering")
+
+png(file="plot3.png", width=480, height=480)
+with(subdata, plot(subdata$DateTime, subdata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+points(x=subdata$DateTime, y=subdata$Sub_metering_2, type="l", col="red")
+points(x=subdata$DateTime, y=subdata$Sub_metering_3, type="l", col="blue")
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = 1)
+dev.off()
